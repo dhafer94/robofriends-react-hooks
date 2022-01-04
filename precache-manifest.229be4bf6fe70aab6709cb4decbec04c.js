0116c4ef50f993ccf99538e16350289be031edb7
Dhafer Alzubairi <dx.dhafer@gmail.com>
2022-01-05 01:31:34 +0200

    Updates

---
+++ precache-manifest.229be4bf6fe70aab6709cb4decbec04c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c553946dfc05ed2642ae577453c343c7",
+    "url": "/dhafer94/robofriends-react-hooks.git/index.html"
+  },
+  {
+    "revision": "8246aa0a2658ab899bdd",
+    "url": "/dhafer94/robofriends-react-hooks.git/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "2bb1dfc79cc8677ddc3e",
+    "url": "/dhafer94/robofriends-react-hooks.git/static/css/main.ee15ac92.chunk.css"
+  },
+  {
+    "revision": "8246aa0a2658ab899bdd",
+    "url": "/dhafer94/robofriends-react-hooks.git/static/js/2.43a9ebcd.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/dhafer94/robofriends-react-hooks.git/static/js/2.43a9ebcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb1dfc79cc8677ddc3e",
+    "url": "/dhafer94/robofriends-react-hooks.git/static/js/main.b5d03d85.chunk.js"
+  },
+  {
+    "revision": "5934356551561a9ae7b0",
+    "url": "/dhafer94/robofriends-react-hooks.git/static/js/runtime-main.4aa75926.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/dhafer94/robofriends-react-hooks.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
